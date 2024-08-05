5cc72a38a98e33af4f7f8c5c135801247166584d
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-04 21:28:01 -0400

    Initialized app, added Poppins and PlayFair_Display fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "100"
+});
+
+const playfair_display = Playfair_Display({subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${poppins.className} ${playfair_display}`}>{children}</body>
     </html>
   );
 }
