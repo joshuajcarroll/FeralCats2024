ee7812465cf8e2bbda32991cbc06818bf07ada50
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-05 20:30:22 -0400

    added About Us section with image of cat to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import catImage from "@/public/isolated-shot-ginger-kitten-sitting-front-white-looking-right.png"
+import catImage from "@/public/cat_icon.png"
+import feralCat from "@/public/matt-seymour-mgLloBtUdvE-unsplash.jpg";
 
 export default function Home() {
   return (
@@ -8,8 +9,16 @@ export default function Home() {
       <div>
         <h1>Friends of Feral Felines</h1>
         <div className={styles.imageContainer}>
-          <Image src={catImage} width={60} height={60} alt="Picture of cat"/>
+          <Image src={catImage} width={40} height={40} alt="Outline of cat" />
         </div>
+        <section>
+          <h1>About Us</h1>
+          <h3>What is Friends of Feral Felines?</h3>
+          <p>
+            Friends of Feral Felines is a resource to help care for feral cats in the community.
+          </p>
+          <Image src={feralCat} width={160} height={160} alt="Picture of cat in foliage" />
+        </section>
         
       </div>
     </main>
