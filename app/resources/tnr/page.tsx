4c5529569e5f847a9fc0f148ce97c154ebe66f4f
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-08 10:19:59 -0400

    Added tnr page and began to add info to the page

---
+++ app/resources/tnr/page.tsx
@@ -0,0 +1,25 @@
+export default function TrapNeuterReturn() {
+    return (
+        <div>
+            <h2>Trap-Neuter-Return (TNR)</h2>
+            <p>
+                TNR is a humane approach to managing feral cat populations. 
+                It involves:
+            </p>
+            <ul>
+                <li>Trapping feral cats</li>
+                <li>Spaying/neutering them to prevent breeding</li>
+                <li>Releasing them back to their colony</li>
+            </ul>
+            <p>Benefits of TNR:</p>
+            <ul>
+                <li>Reduces cat overpopulation</li>
+                <li>Improves community health</li>
+                <li>Stabilizes feral cat colonies</li>
+            </ul>
+            <h3>Local TNR Resources</h3>
+            <p>Here are some organizations that provide TNR services:</p>
+            {/* Add links to local clinics or rescue groups here using `<a>` tags */}
+    </div>
+    )
+}
