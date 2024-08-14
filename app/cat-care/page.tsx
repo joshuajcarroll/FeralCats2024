118772d4cc3ee9f9b7bc479932c7b1b93eb13395
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-13 20:59:42 -0400

    added basic cat care tips to cat care page

--- app/cat-care/page.tsx
+++ app/cat-care/page.tsx
@@ -16,10 +16,14 @@ export default function CatCare() {
       had physical contact with humans or it has diminished over time so that they are no longer 
       accustomed to it. They are fearful of people and may never enjoy being a lap cat or living indoors.</p>
       <p><a href="https://www.alleycat.org/resources/feral-and-stray-cats-an-important-difference/#:~:text=A%20feral%20cat%20is%20an,cat%20or%20enjoy%20living%20indoors.">The Difference Between Strays and Ferals</a></p>
-      <h3>Feeding</h3>
+      <h3>Basic tips for caring for community cats</h3>
+      <h4>Provide Food and Water</h4>
       <p>
-        Provide consistent feeding stations with dry or wet cat food suitable 
-        for outdoor conditions. Maintain clean water.
+      Feed cats a large amount of food once a day on a regular schedule in a specific area. 
+      This helps cats know the area is a reliable food source and are less likely to leave. 
+      You can build or buy a feeding station and keep food dishes in one place for easy cleanup. 
+      Remove food dishes within 30 minutes. Also, provide fresh water that is easy to access near their shelter, 
+      especially in the summer. Dehydrated cats can experience headaches, stomach cramps, and loss of energy.
       </p>
       <h3>Shelter</h3>
       <p>
