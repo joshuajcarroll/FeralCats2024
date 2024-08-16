c5e6bec8965027a2a42fefe9511b7023e4de9df6
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-15 20:09:40 -0400

    created general outline for food assistance programs in resource page

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,5 +1,15 @@
 export default function Resources() {
     return (
-        <h1>Cat Resources</h1>
+        <main>
+            <h1>Resources for Community Cats</h1>
+            <section>
+                <h3>Food Assistance Programs</h3>
+                <p>Brief overview of the importance of proper nutrition for feral cats</p>
+                <p>Information on available food assistance programs (if any) in your area</p>
+                <p>Tips on how to find food pantries or shelters that assist with pet food</p>
+                <p>Guidance on creating a sustainable feeding schedule</p>
+                <p>Information on potential food donors (local businesses, pet stores)</p>
+            </section>
+        </main>
     )
 }
