f04f3c7a3b9efe35705b889edbe53ecd3cfa8611
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-15 22:25:36 -0400

    added image to resources page

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
+import calicoCat from "@/public/pexels-rasul70-18884320.jpg"
+
 export default function Resources() {
     return (
         <main>
             <h1>Resources for Community Cats</h1>
+            <Image src={calicoCat} height={160} width={160} alt="calico cat" />
             <section>
                 <h3>Food Assistance Programs</h3>
                 <p>Brief overview of the importance of proper nutrition for feral cats</p>
